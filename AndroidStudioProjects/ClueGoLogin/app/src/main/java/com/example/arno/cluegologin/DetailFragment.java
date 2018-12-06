52ffbaea41b239593e0535d249285e1198ef8005
Speta643 <Speta643@yahoo.com>
2018-12-06 03:21:34 +0100

    Added Proper Suspect list with details

--- AndroidStudioProjects/ClueGoLogin/app/src/main/java/com/example/arno/cluegologin/DetailFragment.java
+++ AndroidStudioProjects/ClueGoLogin/app/src/main/java/com/example/arno/cluegologin/DetailFragment.java
@@ -12,24 +12,38 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.jar.Attributes;
 import java.util.zip.Inflater;
 
 public class DetailFragment extends Fragment {
 
     public DetailFragment() {
-        // Required empty public constructor
+
+
     }
-    TextView suspect_detail;
+    TextView suspect_detail, suspect_name;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_detail, container, false);
 
+        String name = getArguments().getString("name");
+        String detail = getArguments().getString("detail");
+
+        suspect_name = (TextView)v.findViewById(R.id.suspect_name);
         suspect_detail = (TextView)v.findViewById(R.id.suspect_detail);
+        SetName(name);
+        SetDescription(detail);
         return v;
     }
 
+    public void SetName(String Name){
+
+        suspect_name.setText(Name);
+    }
+
     public void SetDescription(String Description){
+
         suspect_detail.setText(Description);
     }
 
--- AndroidStudioProjects/ClueGoLogin/app/src/main/java/com/example/arno/cluegologin/SuspectFragment.java
+++ AndroidStudioProjects/ClueGoLogin/app/src/main/java/com/example/arno/cluegologin/SuspectFragment.java
@@ -3,6 +3,7 @@ package com.example.arno.cluegologin;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -18,59 +19,47 @@ import java.util.List;
 
 public class SuspectFragment extends Fragment {
 
-   public SuspectFragment(){
+ public SuspectFragment(){
 
-    }
+ }
 
-    String[] menuItems = {"John", "Paul", "Morgan", "Ibrahim", "Arno","Antoine","Jopperman"};
+ public void GoingInDetail(String detail, String name) {
+  FragmentManager manager = getFragmentManager();
+  DetailFragment newDetail = new DetailFragment();
+  Bundle args = new Bundle();
+  args.putString("detail", detail);
+  args.putString("name",name);
+  newDetail.setArguments(args);
+  manager.beginTransaction().replace(R.id.fragment_container, newDetail).addToBackStack(null).commit();
+ }
 
-    @Override
-    public  View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+ String[] menuItems = {"John", "Paul", "Morgan", "Ibrahim", "Arno","Antoine","Jopperman","Sandman"};
+ String [] suspectDetail = {"very long dark hair pink eyes","very good looking boi","very deadly boi","very fat boi","very smeky","very dangerous","deadly to the touch","non cooperating psychopath"};
 
-       View view = inflater.inflate(R.layout.suspect_list, container, false);
+ @Override
+ public  View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
-        ListView listView = (ListView) view.findViewById(R.id.suspect_list_view);
+  View view = inflater.inflate(R.layout.suspect_list, container, false);
 
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                menuItems
+  ListView listView = (ListView) view.findViewById(R.id.suspect_list_view);
 
-        );
+  ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+          getActivity(),
+          android.R.layout.simple_list_item_1,
+          menuItems
 
-        listView.setAdapter(listViewAdapter);
+  );
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-         @Override
-         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-          if(position == 0){
+  listView.setAdapter(listViewAdapter);
+  listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+   @Override
+   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-          }
-          else if(position == 1){
+    GoingInDetail(suspectDetail[position], menuItems[position]);
 
-          }
-          else if(position == 2){
+   }
+  });
 
-          }
-          else if(position == 3){
-
-          }
-          else if(position == 4){
-
-          }
-          else if(position == 5){
-
-          }
-          else if(position == 6){
-
-          }
-          else if(position == 7){
-
-          }
-
-         }
-        });
-
-       return view;
-    }
+  return view;
+ }
 }
